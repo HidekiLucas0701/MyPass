84494ab15ea4d5a1ee64ddbf7a6132e866e2c379
HidekiLucas07 <hideki.lucas0701@gmail.com>
2026-08-11 20:37:24 -0300

    feat(wip): authentication controller, with login endpoint

---
+++ src/main/java/com/java/MyPass/auth/AuthController.java
@@ -0,0 +1,25 @@
+package com.java.MyPass.auth;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+    private final TokenService tokenService;
+
+    public AuthController(TokenService tokenService) {
+        this.tokenService = tokenService;
+    }
+
+    record LoginRequest(String username, String paword) {}
+    record LoginResponse(String token) {}
+
+    @PostMapping("login")
+    public LoginResponse login(Authentication authentication){
+        var jwt = tokenService.generateToken(authentication.getName());
+        return new LoginResponse(jwt);
+    }
+}
+
