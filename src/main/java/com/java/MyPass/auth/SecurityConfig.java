1d12db46b3436fcadb6d5df9b7bad0e127f187e4
HidekiLucas07 <hideki.lucas0701@gmail.com>
2026-08-11 20:14:09 -0300

    feat(wip): authorizing post at login page

--- src/main/java/com/java/MyPass/auth/SecurityConfig.java
+++ src/main/java/com/java/MyPass/auth/SecurityConfig.java
@@ -1,7 +1,11 @@
 package com.java.MyPass.auth;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
@@ -11,4 +15,16 @@ public class SecurityConfig {
 
     @ConfigurationProperties(prefix = "rsa")
     public record RsaKeyProperties(RSAPublicKey publicKey, RSAPrivateKey privateKey) {}
+
+    @Bean
+    SecurityFilterChain config(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .build();
+
+    }
+
 }
