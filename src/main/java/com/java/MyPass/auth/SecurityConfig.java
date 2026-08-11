1bfee7610e640f510d66e59ed86c79aaaf825d11
HidekiLucas07 <hideki.lucas0701@gmail.com>
2026-08-11 20:15:09 -0300

    feat(wip): csrf protection disabled, because it is an API REST

--- src/main/java/com/java/MyPass/auth/SecurityConfig.java
+++ src/main/java/com/java/MyPass/auth/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import java.security.interfaces.RSAPrivateKey;
@@ -23,6 +24,7 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/login").permitAll()
                         .anyRequest().authenticated()
                 )
+                .csrf(AbstractHttpConfigurer::disable)
                 .build();
 
     }
