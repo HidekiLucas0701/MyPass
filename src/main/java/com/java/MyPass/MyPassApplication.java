bb8ac0e018159f3d60b0623af992061acb67823a
HidekiLucas07 <hideki.lucas0701@gmail.com>
2026-08-11 19:25:18 -0300

    Initial Commit

---
+++ src/main/java/com/java/MyPass/MyPassApplication.java
@@ -0,0 +1,13 @@
+package com.java.MyPass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/java/MyPass/MyPassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.MyPass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyPassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
