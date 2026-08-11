6ca1ba5b35b93ee8b6012d7b45cdeec6693c854b
HidekiLucas07 <hideki.lucas0701@gmail.com>
2026-08-11 19:53:26 -0300

    feat(wip): controller with get endpoint

---
+++ src/main/java/com/java/MyPass/pass/PassController.java
@@ -0,0 +1,23 @@
+package com.java.MyPass.pass;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/missions")
+public class PassController {
+
+    private final PassRepository passRepository;
+
+    public PassController(PassRepository passRepository) {
+        this.passRepository = passRepository;
+    }
+
+    @GetMapping
+    List<Pass> list() {
+        return passRepository.findAll();
+    }
+}
